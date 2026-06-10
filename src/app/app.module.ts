6e21d743fb63a1a01b183ddf01bb6375a11472e0
Esteban Castillo <ec2818913@gmail.com>
2026-06-10 13:05:40 -0600

    Creacion de proyecto para protafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortafolioComponent } from './portafolio/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [PortafolioComponent]
 })
 export class AppModule { }
---
+++ src/app/portafolio/portafolio/portafolio.component.ts
@@ -0,0 +1,93 @@
+import {
+  Component,
+  AfterViewInit,
+  OnInit
+} from '@angular/core';
+
+import Typed from 'typed.js';
+import * as AOS from 'aos';
+
+@Component({
+  selector: 'app-portafolio',
+  templateUrl: './portafolio.component.html',
+  styleUrls: ['./portafolio.component.css']
+})
+export class PortafolioComponent implements OnInit, AfterViewInit {
+
+  isDark = true;
+
+  stats = [
+    {
+      value: '10+',
+      title: 'Proyectos'
+    },
+    {
+      value: '4+',
+      title: 'Años de experiencia'
+    },
+    {
+      value: '10+',
+      title: 'Tecnologías'
+    }
+  ];
+
+  technologies = [
+    'Angular',
+    'Java',
+    'Spring Boot',
+    'Git',
+    'GitHub',
+    'Docker',
+    'SQL',
+    'Flutter',
+    'Node',
+    'Microservicios'
+  ];
+
+  projects = [
+    {
+      title: 'Sistema Empresarial',
+      description: 'Proyecto realizado en Angular, Microservicio en Java para la interacción con la base de datos'
+    },
+    {
+      title: 'Aplicaciones Moviles',
+      description: 'Aplicaciones en Flutter con interfaz interactiva y consumo de APIs'
+    },
+    {
+      title: 'GitHub Pages CI/CD',
+      description: 'Deploy automático con workflows en Actions'
+    }
+  ];
+
+  toggleTheme(): void {
+    this.isDark = !this.isDark;
+  }
+
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+
+  ngAfterViewInit(): void {
+
+    new Typed('#typed-text', {
+      strings: [
+        'Full Stack Developer',
+        'Angular Developer',
+        'Java Developer',
+        'Ciberseguridad',
+        'Flutter Developer'
+      ],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true
+    });
+
+  }
+
+  descargarCV(){
+    window.open('https://drive.google.com/file/d/1uiZHaRI7ekNUsKiQolAFIF6t9ytun9FZ/view?usp=sharing', '_blank');
+  }
+}
